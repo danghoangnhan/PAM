836c3406757c9bd7f44c424279f47aad15e48fd9
Danieldu <daniel@jasslin.com>
2023-08-30 21:27:10 +0800

    form daniel

--- config/dir.py
+++ config/dir.py
@@ -3,4 +3,4 @@ import os
 base_directory = os.getcwd()
 static_dir = os.path.join(base_directory, "static")
 audio_dir = os.path.join(static_dir, "audio")
-font_dir = os.path.join(static_dir, "font/Noto_Sans_TC/static/NotoSansTC-Black.ttf")
+font_dir = os.path.join(static_dir, "font","Noto_Sans_TC","static","NotoSansTC-Black.ttf")
